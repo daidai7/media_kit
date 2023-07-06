beb1d6c7e083d4a6f0eaa49ecd469fb7631bbfa6
Daisuke Yamaura <efy.fd3s@gmail.com>
2023-07-07 00:44:41 +0900

    見た目を整えた
    - SeekBarを横並びに
    - ウィンドウサイズが横長の時に小さく鳴りすぎるのを修正

--- media_kit_test/lib/tests/00.onesma_player_test.dart
+++ media_kit_test/lib/tests/00.onesma_player_test.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'package:media_kit/media_kit.dart';
@@ -72,38 +73,47 @@ class _OnesmaPlayerScreenState extends State<OnesmaPlayerScreen> {
             )),
       ]);
 
-  Widget getVideos(BuildContext context) =>
-      MediaQuery.of(context).size.width > MediaQuery.of(context).size.height
-          ? Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: Card(
-                      elevation: 8.0,
-                      clipBehavior: Clip.antiAlias,
-                      margin: const EdgeInsets.all(32.0),
-                      child: getLayeredVideo(
-                        MediaQuery.of(context).size.width,
-                        MediaQuery.of(context).size.width * 9.0 / 16.0,
-                      )),
-                ),
-                Column(
-                  children: [
-                    SeekBar(player: players[0]),
-                    SeekBar(player: players[1])
-                  ],
-                ),
-                const SizedBox(height: 32.0),
-              ],
-            )
-          : Column(children: [
-              getLayeredVideo(
+  Widget getVideos(BuildContext context) {
+    final double windowWidth = MediaQuery.of(context).size.width;
+    return MediaQuery.of(context).size.width >
+            MediaQuery.of(context).size.height
+        ? Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                  child: getLayeredVideo(
                 MediaQuery.of(context).size.width,
                 MediaQuery.of(context).size.width * 9.0 / 16.0,
+              )),
+              Row(
+                children: [
+                  Container(
+                      width: windowWidth / 2,
+                      child: MySeekBar(player: players[0])),
+                  Container(
+                      width: windowWidth / 2,
+                      child: MySeekBar(player: players[1])),
+                ],
               ),
-              SeekBar(player: players[0]),
-              SeekBar(player: players[1])
-            ]);
+            ],
+          )
+        : Column(children: [
+            getLayeredVideo(
+              MediaQuery.of(context).size.width,
+              MediaQuery.of(context).size.width * 9.0 / 16.0,
+            ),
+            Row(
+              children: [
+                Container(
+                    width: windowWidth / 2,
+                    child: MySeekBar(player: players[0])),
+                Container(
+                    width: windowWidth / 2,
+                    child: MySeekBar(player: players[1])),
+              ],
+            ),
+          ]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -119,8 +129,6 @@ class _OnesmaPlayerScreenState extends State<OnesmaPlayerScreen> {
                   Expanded(
                     child: ListView(
                       children: [
-                        const Text('Horizontal',
-                            style: TextStyle(color: Colors.red)),
                         Container(
                           alignment: Alignment.center,
                           width: MediaQuery.of(context).size.width / 2,
@@ -137,7 +145,6 @@ class _OnesmaPlayerScreenState extends State<OnesmaPlayerScreen> {
                 ],
               )
             : ListView(children: [
-                const Text('Vertical', style: TextStyle(color: Colors.red)),
                 getVideos(context),
                 getVideoList(context),
               ]));
@@ -160,6 +167,131 @@ class _OnesmaPlayerScreenState extends State<OnesmaPlayerScreen> {
             ...getAssetsListForIndex(context, 1),
           ]),
         ),
+      ],
+    );
+  }
+}
+
+class MySeekBar extends StatefulWidget {
+  final Player player;
+  const MySeekBar({
+    Key? key,
+    required this.player,
+  }) : super(key: key);
+
+  @override
+  State<MySeekBar> createState() => _MySeekBarState();
+}
+
+class _MySeekBarState extends State<MySeekBar> {
+  late bool playing = widget.player.state.playing;
+  late Duration position = widget.player.state.position;
+  late Duration duration = widget.player.state.duration;
+  late Duration buffer = widget.player.state.buffer;
+
+  bool seeking = false;
+
+  List<StreamSubscription> subscriptions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    playing = widget.player.state.playing;
+    position = widget.player.state.position;
+    duration = widget.player.state.duration;
+    buffer = widget.player.state.buffer;
+    subscriptions.addAll(
+      [
+        widget.player.streams.playing.listen((event) {
+          setState(() {
+            playing = event;
+          });
+        }),
+        widget.player.streams.completed.listen((event) {
+          setState(() {
+            position = Duration.zero;
+          });
+        }),
+        widget.player.streams.position.listen((event) {
+          setState(() {
+            if (!seeking) position = event;
+          });
+        }),
+        widget.player.streams.duration.listen((event) {
+          setState(() {
+            duration = event;
+          });
+        }),
+        widget.player.streams.buffer.listen((event) {
+          setState(() {
+            buffer = event;
+          });
+        }),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final s in subscriptions) {
+      s.cancel();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(width: 48.0),
+            ...[
+              IconButton(
+                onPressed: widget.player.playOrPause,
+                icon: Icon(
+                  playing ? Icons.pause : Icons.play_arrow,
+                ),
+                color: Theme.of(context).primaryColor,
+                iconSize: 36.0,
+              ),
+              const SizedBox(width: 24.0),
+            ],
+            Text(position.toString().substring(2, 7)),
+            Expanded(
+              child: Slider(
+                min: 0.0,
+                max: duration.inMilliseconds.toDouble(),
+                value: position.inMilliseconds.toDouble().clamp(
+                      0.0,
+                      duration.inMilliseconds.toDouble(),
+                    ),
+                secondaryTrackValue: buffer.inMilliseconds.toDouble().clamp(
+                      0.0,
+                      duration.inMilliseconds.toDouble(),
+                    ),
+                onChangeStart: (e) {
+                  seeking = true;
+                },
+                onChanged: position.inMilliseconds > 0
+                    ? (e) {
+                        setState(() {
+                          position = Duration(milliseconds: e ~/ 1);
+                        });
+                      }
+                    : null,
+                onChangeEnd: (e) {
+                  seeking = false;
+                  widget.player.seek(Duration(milliseconds: e ~/ 1));
+                },
+              ),
+            ),
+            Text(duration.toString().substring(2, 7)),
+            const SizedBox(width: 48.0),
+          ],
+        )
       ],
     );
   }

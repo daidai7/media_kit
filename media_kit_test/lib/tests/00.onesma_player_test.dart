d64beef9cceff569d8717709bedff712e5c30826
Daisuke Yamaura <efy.fd3s@gmail.com>
2023-07-06 23:40:38 +0900

    ビデオリストのColumn表示で飛んでいた問題を解決
    初心者には、
    
    List<Widget> HogeFunc => someWidget
    
    ...HogeFunc
    
    で、配列展開されるなんてしらんやん？

--- media_kit_test/lib/tests/00.onesma_player_test.dart
+++ media_kit_test/lib/tests/00.onesma_player_test.dart
@@ -110,38 +110,57 @@ class _OnesmaPlayerScreenState extends State<OnesmaPlayerScreen> {
     final horizontal =
         MediaQuery.of(context).size.width > MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('package:media_kit'),
-      ),
-      body: horizontal
-          ? Row(
-              children: [
-                Expanded(
-                  child: ListView(
-                    children: [
-                      Container(
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width / 2,
-                        height:
-                            MediaQuery.of(context).size.width / 2 * 12.0 / 16.0,
-                        child: getVideos(context),
-                      ),
-                      const Divider(height: 1.0, thickness: 1.0),
-                      ...getAssetsListForIndex(context, 0),
-                      ...getAssetsListForIndex(context, 1),
-                    ],
+        appBar: AppBar(
+          title: const Text('package:media_kit'),
+        ),
+        body: horizontal
+            ? Row(
+                children: [
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        const Text('Horizontal',
+                            style: TextStyle(color: Colors.red)),
+                        Container(
+                          alignment: Alignment.center,
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: MediaQuery.of(context).size.width /
+                              2 *
+                              12.0 /
+                              16.0,
+                          child: getVideos(context),
+                        ),
+                        getVideoList(context)
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            )
-          : ListView(
-              children: [
+                ],
+              )
+            : ListView(children: [
+                const Text('Vertical', style: TextStyle(color: Colors.red)),
                 getVideos(context),
-                const Divider(height: 1.0, thickness: 1.0),
-                ...getAssetsListForIndex(context, 0),
-                ...getAssetsListForIndex(context, 1),
-              ],
-            ),
+                getVideoList(context),
+              ]));
+  }
+
+  Widget getVideoList(BuildContext context) {
+    final double windowWidth = MediaQuery.of(context).size.width;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: windowWidth / 2,
+          child: Column(children: [
+            ...getAssetsListForIndex(context, 0),
+          ]),
+        ),
+        Container(
+          width: windowWidth / 2,
+          child: Column(children: [
+            ...getAssetsListForIndex(context, 1),
+          ]),
+        ),
+      ],
     );
   }
 }

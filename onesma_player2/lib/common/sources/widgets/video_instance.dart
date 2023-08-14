87fbd8b3a3aee7c2b05267f83789b13b102a8934
Daisuke Yamaura <efy.fd3s@gmail.com>
2023-08-14 17:45:57 +0900

    選択したファイルを読み込んで再生できるようにしてみた
    再生ボタンが反映されなかったり未対応のファイルがありそうだったりするエラー処理はまた後で…

--- onesma_player2/lib/common/sources/widgets/video_instance.dart
+++ onesma_player2/lib/common/sources/widgets/video_instance.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:media_kit_video/media_kit_video.dart';
+import 'package:file_picker/file_picker.dart';
 import '../../globals.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as path;
+
+import 'dart:async';
+import 'dart:io';
 
 class VideoInstance extends StatefulWidget {
   final width;
@@ -30,10 +36,11 @@ class _VideoInstanceState extends State<VideoInstance> {
   Widget getVideo() {
     //  実際は（ロード済とか）表示可能になったらだとおもう
     //_isPlayable = player.state.playing;
+    var w = widget.width;
+    var h = w * 9.0 / 16.0;
 
-    return player.state.playing
-        ? Video(
-            controller: controller, width: widget.width, height: widget.height)
+    return _isPlayable
+        ? Video(controller: controller, width: w, height: h)
         : SizedBox(
             child: Card(
                 color: Colors.black,
@@ -42,13 +49,26 @@ class _VideoInstanceState extends State<VideoInstance> {
                       style: TextStyle(fontSize: 20, color: Colors.white),
                       textAlign: TextAlign.center),
                 )),
-            height: widget.width * 9.0 / 16.0,
-            width: widget.width);
+            height: h,
+            width: w);
   }
 
   videoRewind() {
     if (_isPlayable) {
       player.seek(Duration.zero);
+    }
+  }
+
+  openFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      var filePath = result.files.single.path ?? "";
+      print(filePath);
+      player.open(Media(filePath));
+      player.pause();
+      _isPlayable = true;
+    } else {
+      // User canceled the picker
     }
   }
 
@@ -68,10 +88,10 @@ class _VideoInstanceState extends State<VideoInstance> {
           IconButton(
               onPressed: () {
                 setState(() {
-                  _isPlayable = !_isPlayable;
+                  player.playOrPause();
                 });
               },
-              icon: Icon(_isPlayable ? Icons.pause : Icons.play_arrow),
+              icon: Icon(player.state.playing ? Icons.pause : Icons.play_arrow),
               color: _isPlayable ? Colors.blue : Colors.grey,
               iconSize: ICON_SIZE),
           IconButton(
@@ -80,7 +100,11 @@ class _VideoInstanceState extends State<VideoInstance> {
               color: _isPlayable ? Colors.blue : Colors.grey,
               iconSize: ICON_SIZE),
           IconButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  openFile();
+                });
+              },
               icon: Icon(Icons.file_open_rounded),
               color: Colors.blue,
               iconSize: ICON_SIZE),

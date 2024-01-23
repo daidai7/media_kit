0acb2e6c561e0771e0a5e5b504802d8746fe9ff7
Daisuke Yamaura <efy.fd3s@gmail.com>
2024-01-23 16:22:23 +0900

    ミニボタン用の定義を追加
    注：ElevatedButtonはFixedSizeでサイズ変更できない(SizedBoxで囲む必要がある)ので見直し必要

--- onesma_player2/lib/common/sources/widgets/button_style.dart
+++ onesma_player2/lib/common/sources/widgets/button_style.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
 const CONTROL_FONT_SIZE = 15.0;
+const MINI_CONTROL_FONT_SIZE = 10.0;
 
 final ButtonStyle buttonStyle = TextButton.styleFrom(
     padding: EdgeInsets.zero,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
     textStyle: const TextStyle(fontSize: CONTROL_FONT_SIZE),
     disabledForegroundColor: Colors.white, // foreground
     disabledBackgroundColor: Colors.grey,
     foregroundColor: Colors.white, // foreground
     backgroundColor: Colors.blue,
-    fixedSize: const Size(40, 30));
+    fixedSize: const Size(40, 20));
+
+final ButtonStyle miniButtonStyle = TextButton.styleFrom(
+    padding: EdgeInsets.zero,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+    textStyle: const TextStyle(fontSize: MINI_CONTROL_FONT_SIZE),
+    disabledForegroundColor: Colors.white, // foreground
+    disabledBackgroundColor: Colors.grey,
+    foregroundColor: Colors.white, // foreground
+    backgroundColor: Colors.blue);
